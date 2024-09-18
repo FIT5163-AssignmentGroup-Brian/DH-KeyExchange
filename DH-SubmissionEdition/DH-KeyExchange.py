bcad42e826aa1dc91d44c2fb5ee64b81c47e2097
Brian Zhang <brianzhang@192-168-1-102.tpgi.com.au>
2024-09-18 21:03:54 +1000

    finished Person class

--- DH-SubmissionEdition/DH-KeyExchange.py
+++ DH-SubmissionEdition/DH-KeyExchange.py
@@ -1,6 +1,55 @@
 # this is the implementation of our FIT5163 DH Keyexchange
 import random
 import secrets
+from Crypto.Hash import SHA256
+from Crypto.PublicKey import RSA
+from Crypto.Signature import pkcs1_15
+
+# Define a class representing a participant, e.g., Theo or Knew
+class Person:
+    def __init__(self, name):
+        self.name = name  
+        self.P = None     
+        self.G = None     
+        self.private_key = None  
+        self.public_key = None   
+        self.shared_secret = None  
+        self.signature_key = RSA.generate(2048)  
+
+    def set_parameters(self, p, g):
+        # Set the common parameters p and g
+        self.P = p
+        self.G = g
+
+    def generate_keys(self):
+        # Generate private and public keys
+        self.private_key = secrets.randbelow(self.p - 2) + 2  # Random private key in range [2, p-1]
+        self.public_key = pow(self.g, self.private_key, self.p)  
+
+    def sign_data(self, data):
+        # Sign data using RSA private key
+        hasher = SHA256.new(data)  # Hash the data using SHA256
+        signature = pkcs1_15.new(self.signature_key).sign(hasher)  # Generate signature
+        return signature
+
+    def verify_signature(self, data, signature, public_key):
+        # Verify the signature using the counterpart's public key
+        hasher = SHA256.new(data)
+        try:
+            pkcs1_15.new(public_key).verify(hasher, signature)
+            return True  
+        except (ValueError, TypeError):
+            return False  
+
+    def generate_shared_secret(self, other_public_key):
+        # Compute the shared secret key
+        self.shared_secret = pow(other_public_key, self.private_key, self.p)
+        return self.shared_secret
+
+    def __str__(self):
+        # Return a string representation including name, public key, and shared secret
+        return f"{self.name} - Public Key: {self.public_key}, Shared Secret: {self.shared_secret}"
+
 
 
 # key exchange process implementation
@@ -11,8 +60,8 @@ def key_exchange_process():
 
     # Simulate key exchange process
     # To do : need to implemetation Person object class
-    alice = Person("Alice")
-    bob = Person("Bob")
+    theo = Person("Theo")
+    knew = Person("Knew")
 
 
 
