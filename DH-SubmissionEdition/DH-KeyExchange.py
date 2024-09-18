d1e0989aa85b9349a3f7b875eb0b30b11177330d
Brian Zhang <brianzhang@192-168-1-102.tpgi.com.au>
2024-09-18 20:35:04 +1000

    updated initial key exchange process

--- DH-SubmissionEdition/DH-KeyExchange.py
+++ DH-SubmissionEdition/DH-KeyExchange.py
@@ -1,1 +1,30 @@
 # this is the implementation of our FIT5163 DH Keyexchange
+import random
+import secrets
+
+
+# key exchange process implementation
+def key_exchange_process():
+    # Implement two const for computation
+    P = generate_large_prime()
+    G = generate_base(P)
+
+    # Simulate key exchange process
+    # To do : need to implemetation Person object class
+    alice = Person("Alice")
+    bob = Person("Bob")
+
+
+
+
+
+
+
+# main function
+if __name__ == "__main__":
+    # key exchange
+    key_exchange_process()
+
+    # simulate attack 
+
+    # Add PAKE/OPAQUE protocol 
